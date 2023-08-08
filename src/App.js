d67d36cc8393ae3da9d99bf4c363b6bf5903b468
Arun P <jadduarun29@gmail.com>
2023-08-08 13:14:12 +0530

    Completed Code

--- src/App.js
+++ src/App.js
@@ -1,46 +1,48 @@
 import logo from "./logo.svg";
 import "./App.css";
-import { Fragment, useState } from "react";
+import { useState } from "react";
 
 function App() {
-  const [id, setId] = useState(2);
-  const [initialvalues, setInitialvalues] = useState([
-    {
-      id: 1,
-      title: "Hello"
-    }
-  ]);
-  console.log(initialvalues)
+  const [isEditing, setIsEditing] = useState(false);
+  const [editIndex, setEditIndex] = useState(null);
+  const [initialvalues, setInitialvalues] = useState([]);
   const [data, setData] = useState("");
-  const [editItem, setEditItem] = useState(null);
-  function handleSubmit(e) {
-    e.preventDefault();
-    setInitialvalues([...initialvalues, { id: id, title: data }]);
-    setId(id + 1);
+
+  function handleSubmit() {
+    if (data.trim() === "") {
+      alert("Please Enter the Title");
+    }
+    if (data.trim() !== "") {
+      setInitialvalues([...initialvalues, { title: data }]);
+    }
+    setData("")
   }
-  function onDelete(data) {
-    const deleteList = initialvalues.filter((todo)=> todo.id!== data.id);
+
+  function onEdit(index) {
+    setIsEditing(true);
+    setEditIndex(index);
+    setData(initialvalues[index].title);
+  }
+
+  function onDone() {
+    if (data.trim() === "") {
+      alert("Please Enter the Title");
+    }
+    if (data.trim() !== "") {
+      const newArr = [...initialvalues];
+      newArr[editIndex] = { title: data };
+      setInitialvalues(newArr);
+      setIsEditing(false);
+      setData("")
+    }
+  }
+
+  function onDelete(index) {
+    const deleteList = [...initialvalues];
+    deleteList.splice(index, 1);
     setInitialvalues(deleteList);
   }
-  function onEdit(todo) {
-    const newTodo = todo ? [...todo] :todo;
-    editItem(newTodo)
-  }
-  function onEditItem(e){
-    const newName = e.target.value;
-    setEditItem({...editItem,title:newName});
-  }
-  function onDoneEdit(){
-    setInitialvalues(initialvalues=>{
-      return initialvalues.map((todo) => {
-        if(todo.id === editItem.id){
-          return editItem;
-        }
-        return todo;
-      })
-    });
-    setEditItem(null)
-  }
+
   return (
     <div className="App">
       <h1>Notes</h1>
@@ -48,33 +50,35 @@ function App() {
         <fieldset>
           <legend>Notes Taking</legend>
           <div className="form">
-            <form onSubmit={handleSubmit}>
-              <label className="label">Title:</label>
-              <input
-                className="inputBox"
-                value={data}
-                onChange={e => setData(e.target.value)}
-              />
-              <input type="submit" className="button" value="Add" />
-            </form>
+            <label className="label">Title:</label>
+            <input
+              placeholder="Enter Your Todo Here"
+              className="inputBox"
+              value={data}
+              onChange={(e) => setData(e.target.value)}
+            />
+            {!isEditing ? (
+              <button className="button" onClick={() => handleSubmit()}>
+                Add
+              </button>
+            ) : (
+              <button className="button" onClick={() => onDone()}>
+                Save
+              </button>
+            )}
           </div>
         </fieldset>
       </div>
       <ul>
-        {
-        initialvalues.map(todo=> {
-          const isEditing = editItem?.id === todo.id;
+        {initialvalues.map((todo, index) => {
           return (
-            <li key={todo.id}>
-              {!isEditing && <Fragment>{todo.title}</Fragment>}
-              {isEditing && <input value={editItem.title} onChange={onEditItem} />}
-              {!isEditing && <button onClick={() => onEdit(todo)}>Edit</button>}
-              {isEditing && <button onClick={() => onDoneEdit()}>Done</button>}
-              <button onClick={() => onDelete(todo)}>Delete</button>
+            <li key={index}>
+              {todo.title}
+              <button className="edit" onClick={() => onEdit(index)}>Edit</button>
+              <button className="delete" onClick={() => onDelete(index)}>Delete</button>
             </li>
           );
-        })
-        }
+        })}
       </ul>
     </div>
   );
