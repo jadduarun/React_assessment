0b07f6d03ba1205080f00c5ba31a1ce456cf3668
Arun P <jadduarun29@gmail.com>
2023-08-09 09:54:07 +0530

    With Description

--- src/App.js
+++ src/App.js
@@ -7,33 +7,37 @@ function App() {
   const [editIndex, setEditIndex] = useState(null);
   const [initialvalues, setInitialvalues] = useState([]);
   const [data, setData] = useState("");
+  const [content, setContent] = useState("");
 
   function handleSubmit() {
-    if (data.trim() === "") {
-      alert("Please Enter the Title");
+    if (data.trim() === "" || content.trim() === "") {
+      alert("Please Enter the Fields");
     }
-    if (data.trim() !== "") {
-      setInitialvalues([...initialvalues, { title: data }]);
+    if (data.trim() !== "" || content.trim() !== "") {
+      setInitialvalues([...initialvalues, { title: data,description: content }]);
     }
     setData("")
+    setContent("")
   }
 
   function onEdit(index) {
     setIsEditing(true);
     setEditIndex(index);
     setData(initialvalues[index].title);
+    setContent(initialvalues[index].description);
   }
 
   function onDone() {
-    if (data.trim() === "") {
-      alert("Please Enter the Title");
+    if (data.trim() === "" || content.trim() === "") {
+      alert("Please Enter the Fields");
     }
     if (data.trim() !== "") {
       const newArr = [...initialvalues];
-      newArr[editIndex] = { title: data };
+      newArr[editIndex] = { title: data ,description:content};
       setInitialvalues(newArr);
       setIsEditing(false);
       setData("")
+      setContent("")
     }
   }
 
@@ -52,10 +56,18 @@ function App() {
           <div className="form">
             <label className="label">Title:</label>
             <input
-              placeholder="Enter Your Todo Here"
+              placeholder="Enter Your Todo Title"
               className="inputBox"
               value={data}
               onChange={(e) => setData(e.target.value)}
+            />
+            <label className="label">Description:</label>
+            <textarea
+              placeholder="Enter Your Todo Description"
+              className="inputBox"
+              value={content}
+              rows="4"
+              onChange={(e) => setContent(e.target.value)}
             />
             {!isEditing ? (
               <button className="button" onClick={() => handleSubmit()}>
